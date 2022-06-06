4e4fd8384f36dea358d3391ea16661d277bb7099
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-06 15:11:57 +0200

    snack1 working

---
+++ snack1/script.js
@@ -0,0 +1,58 @@
+const containerDiv = document.querySelector(".container");
+const element = document.createElement("div");
+containerDiv.append(element);
+
+const elencoBiciclette = [
+    {
+        nome: "Bianchi",
+        peso: 5,
+    },
+    {
+        nome: "Cinelli",
+        peso: 6,
+    },
+    {
+        nome: "Colnago",
+        peso: 8,
+    },
+    {
+        nome: "Rossignoli",
+        peso: 10,
+    },
+    {
+        nome: "Carrera",
+        peso: 12,
+    },
+    {
+        nome: "Decathlon",
+        peso: 13,
+    }
+];
+
+const {nome, peso} = elencoBiciclette[0];
+
+let biciPiuLeggera = {
+    nome,
+    peso,
+};
+
+for (let i=1; i<elencoBiciclette.length; i++){
+    const {nome, peso} = elencoBiciclette[i];
+
+    if (biciPiuLeggera.peso > peso){
+        biciPiuLeggera = {
+            nome,
+            peso,
+        }
+    }
+    
+}
+
+console.log(biciPiuLeggera)
+
+element.innerHTML = `<h1>${biciPiuLeggera.nome}</h1> \n <h2>${biciPiuLeggera.peso} Kg</h2>`;
+
+
+
+
+
