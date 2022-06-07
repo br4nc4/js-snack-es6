c5e1ca7043f027d29dca78b966a208e333de1bde
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-07 17:00:54 +0200

    snack4 pt.1 working

---
+++ snack4/script.js
@@ -0,0 +1,44 @@
+let students = [
+    { 
+        name: "Marco", 
+        id: 213, 
+        grades: 78 
+    },
+    { 
+        name: "Paola", 
+        id: 110, 
+        grades: 96 
+    },
+    { 
+        name: "Andrea", 
+        id: 250, 
+        grades: 48 
+    },
+    { 
+        name: "Gaia", 
+        id: 145, 
+        grades: 74 
+    },
+    { 
+        name: "Luigi", 
+        id: 196, 
+        grades: 68 
+    },
+    { 
+        name: "Piero", 
+        id: 102, 
+        grades: 50 
+    },
+    { 
+        name: "Francesca", 
+        id: 120, 
+        grades: 84 
+    },
+];
+
+
+const targheStudents = students.map(({name}, i, students) => {
+    return students[i] = name.toUpperCase();
+})
+
+console.log(targheStudents);
