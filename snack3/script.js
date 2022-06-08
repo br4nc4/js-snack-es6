7bb9751e95a7ec2782e76dc8c10b77e327808353
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-08 09:46:13 +0200

    snack3 from lesson 0806

--- snack3/script.js
+++ snack3/script.js
@@ -7,3 +7,27 @@ const myArray = [
     "Chiara"
 ];
 
+//numeri che indicano l'indice minimo e il massimo
+const numMin = 2;
+const numMax = 4;
+
+const nuovoArray =[];
+
+myArray.forEach((guest, i) =>{
+    //se il valore di i è compreso tra nummin e nummax
+    //pushiamo l'elemento nel nuovo array
+
+    if(i>= numMin && i <= numMax){
+        nuovoArray.push(guest);
+    }
+})
+
+console.log(nuovoArray);
+
+const nuovoArrayFilter = myArray.filter((guest, i) =>{
+    if(i>= numMin && i <= numMax){
+        return true
+    }
+});
+
+console.log(nuovoArrayFilter);
